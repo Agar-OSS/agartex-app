81f7de8e3fae67bad42d968fd25c6d0c24733fdd
Hubert Zięba <hub.zieba@student.uj.edu.pl>
2023-04-06 14:47:49 +0200

    Apply suggestions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 function generateConfigJs() {
     echo "/*<![CDATA[*/";
     echo "window.env = window.env || {};";
-    for i in `env | grep '^REACT_APP_'`
+    for i in $(env | grep '^REACT_APP_')
     do
         key=$(echo "$i" | cut -d"=" -f1);
         val=$(echo "$i" | cut -d"=" -f2);
-        echo "window.env.${key}='${val}' ;";
+        echo "window.env.${key}='${val}';";
     done
     echo "/*]]>*/";
 }
