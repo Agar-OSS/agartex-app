19f4c20c8f52e681a4e413e4fd9d37fc17111181
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-06-15 23:22:09 +0200

    Dynamic projects page without error handling

--- src/pages/projects/service/projects-service.ts
+++ src/pages/projects/service/projects-service.ts
@@ -1,41 +1,28 @@
+import { AGARTEX_SERVICE_PROJECTS_URL } from '@constants';
 import { Project } from '@model';
+import axios from 'axios';
 
-let testProjectList: Project[] = Array(30).fill(0).map((_, index) => {
-  return {
-    projectId: `project_${index+1}`,
-    name: `Project ${index+1}`,
-    created: Date.now() - 24*60*60*1000,
-    modified: Date.now() - (index+1)*60*1000,
-    owner: 'rybahubert'
-  };
-});
+interface RawProject {
+  project_id: string,
+  name: string,
+  owner: string
+}
 
 /* Create project page of the given name and return ID of new created resource */
 export const createProject = async (projectName: string): Promise<string> => {
-  // TODO: replace with 
-  // axios.post(AGARTEX_SERVICE_PROJECTS_URL, { projectName })
-  await new Promise(res => {
-    setTimeout(res, 1000);
-  });
-
-  const newProject: Project = {
-    projectId: `id_${projectName.toLowerCase()}`,
-    name: projectName,
-    created: Date.now(),
-    modified: Date.now(),
-    owner: 'current user'
-  };
-
-  testProjectList = [newProject, ...testProjectList];
-  return newProject.projectId;
+  const res = await axios.post<RawProject>(AGARTEX_SERVICE_PROJECTS_URL, { name: projectName });
+  return res.data.project_id;
 };
 
 /* Fetch projects list for the current user */
 export const fetchProjectList = async (): Promise<Project[]> => {
-  await new Promise(res => {
-    setTimeout(res, 1000);
-  });
-  // TODO: replace with
-  // axios.get(AGARTEX_SERIVCE_PROJECTS_URL, {});
-  return testProjectList;
+  const res = await axios.get<RawProject[]>(AGARTEX_SERVICE_PROJECTS_URL);
+  // TODO: populate create and modified from backend
+  return res.data.map((raw) => ({
+    projectId: raw.project_id,
+    name: raw.name,
+    owner: raw.owner,
+    created: Date.now(),
+    modified: Date.now()
+  }));
 };
